a4c55cfaca365969e03735930c1a0e17ca645bd3
YawDev <nik.xeiladakis@gmail.com>
2024-07-24 15:01:39 +0300

    do not continue if file isn't a dll

--- injector.py
+++ injector.py
@@ -17,44 +17,44 @@ while True:
                 print("ERROR: Selected file is not a DLL!")
                 print("\n[>] Press any key to retry...")
                 os.system("pause > nul")
-
-            process_to_inject = str(input("[>] Enter a process to inject into.\n>> "))
-            if not process_to_inject.endswith(".exe"): process_to_inject = process_to_inject + ".exe"
-
-            process_found = False
-            print("\n[+] Searching for game process...")
-            for process in psutil.process_iter(attrs=['pid', 'name']):
-                try:
-                    if process.info['name'] == process_to_inject:
-                        process_id = process.info['pid']
-                        process_name = process.info['name']
-                        process_found = True
-                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                    pass
-            
-            if process_found:
-                #dll_to_inject = os.path.basename(dll_to_inject)
-                print(f"[+] Process {process_name} located at PID:{process_id}")
-                print("\nPress any key to inject!")
-                os.system("pause > nul")
-
-                try:
-                    inject(process_id, dll_to_inject)
-                    print(f"[+] Injected {dll_to_inject} into {process_name}:{process_id}")
-                    print("\n[+] Enjoy!")
-
-                except Exception as e:
-                    os.system("cls")
-                    print("\nERROR: Injection failed!\n[-] Check error.log for more information.")
-                    with open("error.log", "w") as errorfile:
-                        errorfile.write(str(e))
-                    print("\n[>] Press any key to retry...")
-                os.system("pause > nul")
-
             else:
-                print("[-] Failed to locate the game process.")
-                print("\n[>] Press any key to retry...")
-                os.system("pause > nul")
+                process_to_inject = str(input("[>] Enter a process to inject into.\n>> "))
+                if not process_to_inject.endswith(".exe"): process_to_inject = process_to_inject + ".exe"
+
+                process_found = False
+                print("\n[+] Searching for game process...")
+                for process in psutil.process_iter(attrs=['pid', 'name']):
+                    try:
+                        if process.info['name'] == process_to_inject:
+                            process_id = process.info['pid']
+                            process_name = process.info['name']
+                            process_found = True
+                    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                        pass
+                    
+                if process_found:
+                    #dll_to_inject = os.path.basename(dll_to_inject)
+                    print(f"[+] Process {process_name} located at PID:{process_id}")
+                    print("\nPress any key to inject!")
+                    os.system("pause > nul")
+
+                    try:
+                        inject(process_id, dll_to_inject)
+                        print(f"[+] Injected {dll_to_inject} into {process_name}:{process_id}")
+                        print("\n[+] Enjoy!")
+
+                    except Exception as e:
+                        os.system("cls")
+                        print("\nERROR: Injection failed!\n[-] Check error.log for more information.")
+                        with open("error.log", "w") as errorfile:
+                            errorfile.write(str(e))
+                        print("\n[>] Press any key to retry...")
+                    os.system("pause > nul")
+
+                else:
+                    print("[-] Failed to locate the game process.")
+                    print("\n[>] Press any key to retry...")
+                    os.system("pause > nul")
 
         case '2':
             os.system("start https://github.com/Yaw-Dev/ParadiseInjector")
